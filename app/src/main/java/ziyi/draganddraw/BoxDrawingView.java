abec05cd0e01fe2f38a83e5e8209db79a408ade3
sohnyi <sohnyi@163.com>
2016-11-03 00:54:39 +0800

    Initial commit AndroidProgramming2e demo in ch29 with no Challenge practice

---
+++ app/src/main/java/ziyi/draganddraw/BoxDrawingView.java
@@ -0,0 +1,89 @@
+package ziyi.draganddraw;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ziyi on 2016/11/3.
+ */
+
+public class BoxDrawingView extends View {
+    private static final String TAG = "BoxDrawingView";
+
+    private Paint mBoxPaint;
+    private Paint mBackgroundPaint;
+
+    private Box mCurrentBox;
+    private List<Box> mBoxen = new ArrayList<>();
+
+    public BoxDrawingView(Context context) {
+        this(context, null);
+    }
+
+    public BoxDrawingView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+
+        mBoxPaint = new Paint();
+        mBoxPaint.setColor(0x22ff0000);
+
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setColor(0xfff8efe0);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF pointF = new PointF(event.getX(), event.getY());
+        String action = "";
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                mCurrentBox = new Box(pointF);
+                mBoxen.add(mCurrentBox);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                if (mCurrentBox != null) {
+                    mCurrentBox.setCurrent(pointF);
+                    invalidate();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                mCurrentBox = null;
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                mCurrentBox = null;
+                break;
+        }
+
+        Log.d(TAG, action + "action (x,y):(" + pointF.x +"," + pointF.y +")");
+
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPaint(mBackgroundPaint);
+
+        for (Box box : mBoxen) {
+            float left = Math.min(box.getOrigin().x, box.getCurrent().x);
+            float right = Math.max(box.getOrigin().y, box.getCurrent().y);
+            float top = Math.min(box.getOrigin().y, box.getCurrent().y);
+            float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+
+            canvas.drawRect(left, top, right, bottom, mBoxPaint);
+
+        }
+    }
+}
